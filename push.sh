78ec3b9a361865c7d63f9fe441d35f5a7b772d7b
peter <hfutqiufei@163.com>
2020-08-15 13:52:15 +0800

    add blog

--- push.sh
+++ push.sh
@@ -13,6 +13,10 @@ git push
 echo "blog数据已经更新到netlify！"
 
 
+# 在blog这个git内，再生成一个qiufei.github.io的git
+# 从网站下载空的qiufei.github.io.git到blog文件夹下，
+# 然后让blog的内容发布到qiufei.github.io文件夹
+
 cd ./qiufei.github.io
 
 git add --all
